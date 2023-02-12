77c7f3be5c8628a1fe0f290d3f2857d6261e0ed8
avinashnadendla <avinash.nadenla@system73.com>
2023-02-13 02:54:33 +0530

    Trusted origins changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
 
 # Application definition
 
